23a0b75cda6e88204c9d9f8ace7b40b2cb97caa5
deek3 <111648839+deek3@users.noreply.github.com>
2024-01-31 10:19:19 +0530

    Add files via upload

---
+++ .vscode/app.js
@@ -0,0 +1,10 @@
+
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+ScrollReveal().reveal('.text,.heading',{origin:'top'});
+ScrollReveal().reveal('.home-img img,.about-img img,.services-container,.portfolio-box,.contact form',{origin:'bottom'})
+ScrollReveal().reveal('.text h1,.about-img img',{origin:'left'});
